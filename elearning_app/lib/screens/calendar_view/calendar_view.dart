55f592e7aa3a1d85a8a90f2a0208790453f0aa0e
vhyenn <84668078+VienHaiYen@users.noreply.github.com>
2023-12-25 20:14:28 +0700

    add DefaultTabController  to calendar view

--- elearning_app/lib/screens/calendar_view/calendar_view.dart
+++ elearning_app/lib/screens/calendar_view/calendar_view.dart
@@ -14,62 +14,117 @@ class CalendarView extends StatefulWidget {
 }
 
 class _CalendarViewState extends State<CalendarView> {
-  TextEditingController nameC = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
+    // return Text("456");
     return Scaffold(
       appBar: MyAppBar(),
       body: Stack(
         children: [
           const Positioned(bottom: 20, right: 20, child: GroupFixedButton()),
-          Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
-              child: ListView(children: [
-                Container(
-                  padding: const EdgeInsets.only(top: 8),
-                  alignment: Alignment.topLeft,
-                  child: const Icon(
-                    Icons.calendar_month,
-                    size: 120,
-                  ),
-                ),
-                Container(
-                  padding: const EdgeInsets.symmetric(vertical: 8),
-                  child: Text(
-                    AppLocalizations.of(context)!.booked_schedule,
-                    style: const TextStyle(
-                        fontSize: 25, fontWeight: FontWeight.w600),
-                  ),
-                ),
-                // TODO:
+          Column(
+            children: [
+              Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Row(children: [
+                    Container(
+                      padding: const EdgeInsets.only(top: 8),
+                      alignment: Alignment.topLeft,
+                      child: const Icon(
+                        Icons.calendar_month,
+                        size: 120,
+                      ),
+                    ),
+                    Flexible(
+                      child: Column(
+                        children: [
+                          Container(
+                            alignment: Alignment.topLeft,
+                            padding: const EdgeInsets.symmetric(vertical: 8),
+                            child: Text(
+                              AppLocalizations.of(context)!.booked_schedule,
+                              style: const TextStyle(
+                                  fontSize: 25, fontWeight: FontWeight.w600),
+                            ),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(top: 8),
+                            padding: const EdgeInsets.only(
+                                top: 4, bottom: 8, left: 16),
+                            decoration: const BoxDecoration(
+                                border: Border(
+                                    left: BorderSide(
+                                        width: 4,
+                                        color:
+                                            Color.fromARGB(255, 87, 86, 86)))),
+                            child: Text(
+                              AppLocalizations.of(context)!.booked_time_frame,
+                              style: const TextStyle(fontSize: 16),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ])),
 
-                Container(
-                  margin: const EdgeInsets.only(top: 8),
-                  padding: const EdgeInsets.only(top: 4, bottom: 8, left: 16),
-                  decoration: const BoxDecoration(
-                      border: Border(
-                          left: BorderSide(
-                              width: 4,
-                              color: Color.fromARGB(255, 87, 86, 86)))),
-                  // TODO: Làm thanh dọc đoạn văn bản trên
-                  child: Text(
-                    AppLocalizations.of(context)!.booked_time_frame,
-                    style: const TextStyle(fontSize: 16),
+              Flexible(
+                child: DefaultTabController(
+                  length: 2,
+                  child: Column(
+                    children: [
+                      TabBar(
+                        tabs: [
+                          Tab(
+                            icon: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(
+                                  Icons.timer_outlined,
+                                  color: Colors.blue,
+                                ),
+                                SizedBox(width: 8),
+                                Text(
+                                  'Upcoming',
+                                  style: TextStyle(color: Colors.blue),
+                                )
+                              ],
+                            ),
+                          ),
+                          Tab(
+                            icon: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(
+                                  Icons.history_outlined,
+                                  color: Colors.blue,
+                                ),
+                                SizedBox(width: 8),
+                                Text(
+                                  'History',
+                                  style: TextStyle(color: Colors.blue),
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      Expanded(
+                        child: TabBarView(
+                          children: [
+                            Text("1"),
+                            Text("2"),
+                            // UpcomingView(),
+                            // HistoryView(),
+                          ],
+                        ),
+                      )
+                    ],
                   ),
                 ),
-                Container(
-                  padding: const EdgeInsets.only(top: 9),
-                  child: const Text(
-                    "Latest Book",
-                    style: TextStyle(fontSize: 17, fontWeight: FontWeight.w600),
-                  ),
-                ),
-                // Lastest book not start
-                Container(),
-                // TODO: Dropdown of lession item
-                const CalendarItem()
-              ])),
+              ),
+              // const CalendarItem()
+            ],
+          ),
         ],
       ),
     );
--- elearning_app/lib/screens/menu_list/menu_list.dart
+++ elearning_app/lib/screens/menu_list/menu_list.dart
@@ -8,66 +8,60 @@ import 'package:elearning_app/screens/menu_list/widgets/user_tile.dart';
 import 'package:elearning_app/widgets/group_fixed_button.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class MenuList extends StatelessWidget {
+class MenuList extends StatefulWidget {
   MenuList({super.key});
 
+  @override
+  State<MenuList> createState() => _MenuListState();
+}
+
+class _MenuListState extends State<MenuList> {
   TextEditingController courseC = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
         appBar: MyAppBar(),
         body: Stack(children: [
-          // TutorTile(),
-          MenuListBody(),
-          Positioned(bottom: 20, right: 20, child: GroupFixedButton())
+          Positioned(bottom: 20, right: 20, child: GroupFixedButton()),
+          ListView(
+            children: [
+              UserTile(),
+              MenuTile(
+                  title: AppLocalizations.of(context)!.recurring_schedule,
+                  icon: const Icon(Icons.calendar_month),
+                  route: AppRouter.myCalendar),
+              MenuTile(
+                  title: AppLocalizations.of(context)!.tutor,
+                  icon: const Icon(Icons.calendar_month),
+                  route: AppRouter.findingTutor),
+              MenuTile(
+                  title: AppLocalizations.of(context)!.schedule,
+                  icon: const Icon(Icons.calendar_view_week),
+                  route: AppRouter.myCalendar),
+              MenuTile(
+                  title: "Lịch sử",
+                  icon: const Icon(Icons.history),
+                  route: AppRouter.myHistory),
+              MenuTile(
+                title: AppLocalizations.of(context)!.course,
+                icon: const Icon(Icons.school),
+                route: AppRouter.courses,
+              ),
+              MenuTile(
+                  title: AppLocalizations.of(context)!.my_courses,
+                  icon: const Icon(Icons.book),
+                  route: AppRouter.myCourses),
+              MenuTile(
+                  title: AppLocalizations.of(context)!.tutor_register,
+                  icon: const Icon(Icons.calendar_month),
+                  route: AppRouter.becomeTutor),
+              MenuTile(
+                  title: "Đăng xuất",
+                  icon: const Icon(Icons.logout_outlined),
+                  route: AppRouter.logIn),
+            ],
+          )
         ]));
-  }
-}
-
-class MenuListBody extends StatelessWidget {
-  const MenuListBody({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: Set fited icon
-    return ListView(
-      children: [
-        UserTile(),
-        MenuTile(
-            title: AppLocalizations.of(context)!.recurring_schedule,
-            icon: const Icon(Icons.calendar_month),
-            route: AppRouter.myCalendar),
-        MenuTile(
-            title: AppLocalizations.of(context)!.tutor,
-            icon: const Icon(Icons.calendar_month),
-            route: AppRouter.findingTutor),
-        MenuTile(
-            title: AppLocalizations.of(context)!.schedule,
-            icon: const Icon(Icons.calendar_view_week),
-            route: AppRouter.myCalendar),
-        MenuTile(
-            title: "Lịch sử",
-            icon: const Icon(Icons.history),
-            route: AppRouter.myHistory),
-        MenuTile(
-          title: AppLocalizations.of(context)!.course,
-          icon: const Icon(Icons.school),
-          route: AppRouter.courses,
-        ),
-        MenuTile(
-            title: AppLocalizations.of(context)!.my_courses,
-            icon: const Icon(Icons.book),
-            route: AppRouter.myCourses),
-        MenuTile(
-            title: AppLocalizations.of(context)!.tutor_register,
-            icon: const Icon(Icons.calendar_month),
-            route: AppRouter.becomeTutor),
-        MenuTile(
-            title: "Đăng xuất",
-            icon: const Icon(Icons.logout_outlined),
-            route: AppRouter.logIn),
-      ],
-    );
   }
 }
