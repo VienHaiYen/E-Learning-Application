1e98ca59841935c5d2573bd79eb9684700d0f9c9
vhyenn <84668078+VienHaiYen@users.noreply.github.com>
2023-12-25 13:49:21 +0700

    change state of course detail

--- elearning_app/lib/screens/course_detail/course_detail.dart
+++ elearning_app/lib/screens/course_detail/course_detail.dart
@@ -1,253 +1,269 @@
 // ignore_for_file: must_be_immutable
 
+import 'package:elearning_app/models/course/course.dart';
 import 'package:elearning_app/widgets/my_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:elearning_app/widgets/course_item.dart';
-
-import 'package:elearning_app/screens/tutor_view/tutor_view.dart';
+import 'package:elearning_app/services/course_service.dart';
 import 'package:elearning_app/widgets/group_fixed_button.dart';
-
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class CourseDetail extends StatelessWidget {
+class CourseDetail extends StatefulWidget {
   CourseDetail({super.key, required this.courseId});
 
   final String courseId;
 
+  @override
+  State<CourseDetail> createState() => _CourseDetailState();
+}
+
+class _CourseDetailState extends State<CourseDetail> {
   TextEditingController courseC = TextEditingController();
+
+  late final String courseId;
+  late final Course courseDetail;
+
+  bool _isLoading = true;
+
+  Future<void> _fetchCourseDetail(String token) async {
+    final result = await CourseService.getCourseDetailById(
+      token: token,
+      courseId: courseId,
+    );
+
+    setState(() {
+      courseDetail = result;
+      _isLoading = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        appBar: MyAppBar(),
-        body: Stack(children: [
-          CourseDetailBody(),
-          Positioned(bottom: 20, right: 20, child: GroupFixedButton())
-        ]));
-  }
-}
+    const topics = [
+      'The Internet',
+      'Artificial Intelligence',
+      'Social Media',
+      'Internet Privacy',
+      'Live Streaming'
+    ];
 
-class CourseDetailBody extends StatelessWidget {
-  const CourseDetailBody({Key? key}) : super(key: key);
-
-  static const topics = [
-    'The Internet',
-    'Artificial Intelligence',
-    'Social Media',
-    'Internet Privacy',
-    'Live Streaming'
-  ];
-
-  Widget header(BuildContext context, String header) {
-    return Row(
-      children: [
-        const SizedBox(
-          width: 24,
-          child: Divider(),
-        ),
-        const SizedBox(
-          width: 8,
-        ),
-        Text(
-          header,
-          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-        ),
-        const SizedBox(
-          width: 8,
-        ),
-        const Expanded(child: Divider())
-      ],
-    );
-  }
-
-  Widget subHeading(BuildContext context, IconData icon, String title) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8),
-      child: Row(
+    Widget header(BuildContext context, String header) {
+      return Row(
         children: [
-          Icon(
-            icon,
-            size: 19,
-            color: const Color.fromARGB(255, 54, 120, 244),
+          const SizedBox(
+            width: 24,
+            child: Divider(),
           ),
           const SizedBox(
             width: 8,
           ),
-          Text(title,
-              style: const TextStyle(
-                fontSize: 16,
-              )),
+          Text(
+            header,
+            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            width: 8,
+          ),
+          const Expanded(child: Divider())
         ],
-      ),
-    );
-  }
+      );
+    }
 
-  Widget description(
-      BuildContext context, IconData icon, String title, String content) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const SizedBox(
-          height: 8,
-        ),
-        Row(
+    Widget subHeading(BuildContext context, IconData icon, String title) {
+      return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        child: Row(
           children: [
             Icon(
               icon,
-              size: 18,
-              color: const Color.fromARGB(255, 244, 54, 54),
+              size: 19,
+              color: const Color.fromARGB(255, 54, 120, 244),
             ),
             const SizedBox(
               width: 8,
             ),
-            Text(
-              title,
-              style: const TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
-            ),
+            Text(title,
+                style: const TextStyle(
+                  fontSize: 16,
+                )),
           ],
         ),
-        Padding(
-          padding: const EdgeInsets.only(left: 32, top: 4),
-          child: Text(
-            content,
-            textAlign: TextAlign.left,
-            style: const TextStyle(fontWeight: FontWeight.w100),
-          ),
-        ),
-      ],
-    );
-  }
+      );
+    }
 
-  Widget topicList() {
-    return ListView.builder(
-      shrinkWrap: true,
-      scrollDirection: Axis.vertical,
-      physics: const NeverScrollableScrollPhysics(),
-      itemCount: topics.length,
-      itemBuilder: (context, index) {
-        return GestureDetector(
-          onTap: () {
-            // Navigator.push(context,
-            // MaterialPageRoute(builder: (context) => const LogIn()));
-          },
-          child: Card(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 36),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('${index + 1}.'),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  Text(topics[index],
-                      style: const TextStyle(
-                          fontSize: 16, fontWeight: FontWeight.bold)),
-                ],
-              ),
-            ),
-          ),
-        );
-      },
-    );
-  }
-
-  Widget suggestedTutor(BuildContext context, String tutorName) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text(tutorName),
-        TextButton(
-            onPressed: () {
-              // Navigator.push(context,
-              //     MaterialPageRoute(builder: (context) => const TutorView()));
-            },
-            child: Text(AppLocalizations.of(context)!.more_info)),
-      ],
-    );
-  }
-
-  Widget suggestedTutorList() {
-    return ListView.builder(
-      shrinkWrap: true,
-      scrollDirection: Axis.vertical,
-      physics: const NeverScrollableScrollPhysics(),
-      itemCount: 12,
-      itemBuilder: (context, index) {
-        return suggestedTutor(context, '${index + 1}. April');
-      },
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
+    Widget description(
+        BuildContext context, IconData icon, String title, String content) {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          CourseItem(
-              bottomWidget: Row(
+          const SizedBox(
+            height: 8,
+          ),
+          Row(
             children: [
-              Expanded(
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('Khám phá'),
-                ),
+              Icon(
+                icon,
+                size: 18,
+                color: const Color.fromARGB(255, 244, 54, 54),
+              ),
+              const SizedBox(
+                width: 8,
+              ),
+              Text(
+                title,
+                style:
+                    const TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
               ),
             ],
-          )),
-
-          // Overview
-          header(context, 'Overview'),
-          const SizedBox(
-            height: 8,
           ),
-          description(
-              context,
-              Icons.question_mark_outlined,
-              'Why take this course',
-              "Our world is rapidly changing thanks to new technology, and the vocabulary needed to discuss modern life is evolving almost daily. In this course you will learn the most up-to-date terminology from expertly crafted lessons as well from your native-speaking tutor."),
-          const SizedBox(
-            height: 8,
+          Padding(
+            padding: const EdgeInsets.only(left: 32, top: 4),
+            child: Text(
+              content,
+              textAlign: TextAlign.left,
+              style: const TextStyle(fontWeight: FontWeight.w100),
+            ),
           ),
-          description(
-              context,
-              Icons.question_mark_outlined,
-              'What will you be able to do',
-              "You will learn vocabulary related to timely topics like remote work, artificial intelligence, online privacy, and more. In addition to discussion questions, you will practice intermediate level speaking tasks such as using data to describe trends."),
-          const SizedBox(
-            height: 16,
-          ),
-
-          // Experience level
-          header(context, 'Experience Level'),
-          const SizedBox(
-            height: 8,
-          ),
-          subHeading(context, Icons.person_add_alt_1_outlined, 'Beginner'),
-
-          // Course length
-          header(context, 'Course Length'),
-          const SizedBox(
-            height: 8,
-          ),
-          subHeading(context, Icons.my_library_books_outlined, '5 topics'),
-
-          // List topic
-          header(context, 'List Topic'),
-          topicList(),
-          const SizedBox(
-            height: 16,
-          ),
-
-          // Suggested tutors
-          header(context, 'Suggested Tutors'),
-          const SizedBox(
-            height: 8,
-          ),
-          suggestedTutorList()
         ],
-      ),
-    );
+      );
+    }
+
+    Widget topicList() {
+      return ListView.builder(
+        shrinkWrap: true,
+        scrollDirection: Axis.vertical,
+        physics: const NeverScrollableScrollPhysics(),
+        itemCount: topics.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              // Navigator.push(context,
+              // MaterialPageRoute(builder: (context) => const LogIn()));
+            },
+            child: Card(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 36),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('${index + 1}.'),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Text(topics[index],
+                        style: const TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold)),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      );
+    }
+
+    Widget suggestedTutor(BuildContext context, String tutorName) {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(tutorName),
+          TextButton(
+              onPressed: () {
+                // Navigator.push(context,
+                //     MaterialPageRoute(builder: (context) => const TutorView()));
+              },
+              child: Text(AppLocalizations.of(context)!.more_info)),
+        ],
+      );
+    }
+
+    Widget suggestedTutorList() {
+      return ListView.builder(
+        shrinkWrap: true,
+        scrollDirection: Axis.vertical,
+        physics: const NeverScrollableScrollPhysics(),
+        itemCount: 12,
+        itemBuilder: (context, index) {
+          return suggestedTutor(context, '${index + 1}. April');
+        },
+      );
+    }
+
+    return Scaffold(
+        appBar: MyAppBar(),
+        body: Stack(children: [
+          Positioned(bottom: 20, right: 20, child: GroupFixedButton()),
+          SingleChildScrollView(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              children: [
+                CourseItem(
+                    bottomWidget: Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: const Text('Khám phá'),
+                      ),
+                    ),
+                  ],
+                )),
+
+                // Overview
+                header(context, 'Overview'),
+                const SizedBox(
+                  height: 8,
+                ),
+                description(
+                    context,
+                    Icons.question_mark_outlined,
+                    'Why take this course',
+                    "Our world is rapidly changing thanks to new technology, and the vocabulary needed to discuss modern life is evolving almost daily. In this course you will learn the most up-to-date terminology from expertly crafted lessons as well from your native-speaking tutor."),
+                const SizedBox(
+                  height: 8,
+                ),
+                description(
+                    context,
+                    Icons.question_mark_outlined,
+                    'What will you be able to do',
+                    "You will learn vocabulary related to timely topics like remote work, artificial intelligence, online privacy, and more. In addition to discussion questions, you will practice intermediate level speaking tasks such as using data to describe trends."),
+                const SizedBox(
+                  height: 16,
+                ),
+
+                // Experience level
+                header(context, 'Experience Level'),
+                const SizedBox(
+                  height: 8,
+                ),
+                subHeading(
+                    context, Icons.person_add_alt_1_outlined, 'Beginner'),
+
+                // Course length
+                header(context, 'Course Length'),
+                const SizedBox(
+                  height: 8,
+                ),
+                subHeading(
+                    context, Icons.my_library_books_outlined, '5 topics'),
+
+                // List topic
+                header(context, 'List Topic'),
+                topicList(),
+                const SizedBox(
+                  height: 16,
+                ),
+
+                // Suggested tutors
+                header(context, 'Suggested Tutors'),
+                const SizedBox(
+                  height: 8,
+                ),
+                suggestedTutorList()
+              ],
+            ),
+          ),
+        ]));
   }
 }
